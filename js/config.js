94c09d0f37ae1e7f97b8574ee98b1d5e22b1e359
alexandre an <an.alexcore@gmail.com>
2019-10-08 12:17:08 +0100

    fix

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: ' eu-west-1_PlQQSyMK6', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'eu-west-1_PlQQSyMK6', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '6nhqiffh94kuut7p1n0r1mh42q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-1' // e.g. us-east-2
     },
